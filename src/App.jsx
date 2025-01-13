e8a2cd2c9e870d03b7b8a578a26a2c62425c30fa
daenikon <dkondratiuk06@gmail.com>
2025-01-13 21:51:52 +0100

    setup vitejs

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "leaflet/dist/leaflet.css"; // Import Leaflet's CSS
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+function App() {
+
+  return (
+    <>
+      <h1>Test</h1>
+    </>
+  );
+}
+
+export default App;
+
