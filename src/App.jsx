e0e1b8f13ac9416f027685f61dad336667f6f12a
daenikon <dkondratiuk06@gmail.com>
2025-01-14 18:49:01 +0100

    add polyline logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "leaflet/dist/leaflet.css";
 import "./index.css";
-import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMapEvents, Polyline } from "react-leaflet";
 
 function App() {
-    const MapClickLogger = () => {
-        useMapEvents({
-          click: (e) => {
-            console.log("Coordinates clicked:", e.latlng);
-          },
+  const [markers, setMarkers] = useState([]);
+  const [lines, setLines] = useState([]);
+
+  const MapClickHandler = () => {
+    useMapEvents({
+      click: (e) => {
+        const { lat, lng } = e.latlng;
+
+        setMarkers((prevMarkers) => {
+          const newMarkers = [...prevMarkers, { lat, lng }];
+
+          if (newMarkers.length > 1) {
+            const prevNode = newMarkers[newMarkers.length - 2];
+            setLines((prevLines) => [...prevLines, [prevNode, { lat, lng }]]);
+          }
+
+          return newMarkers;
         });
-        return null; // to not render any ui
-    };
+
+        console.log("Coordinates clicked:", { lat, lng });
+        console.log("Array of current nodes:", markers);
+        console.log("Array of current lines:", lines);
+      },
+    });
+    return null; // No UI rendered
+  };
 
   return (
     <div>
-      <h2 style={{ textAlign: "center", margin: "20px 0" }}>
-        TSP Visualizer
-      </h2>
+      <div style={{display: "flex", justifyContent: "space-between", alignItems: "center", margin: "5px 40px"}}>
+        <h2 style={{ textAlign: "center", margin: "20px 0" }}>TSP Visualizer</h2>
+        <button style={{width: "80px", height: "40px", border: "1px solid lime"}}>Run</button>
+      </div>
       <MapContainer
         center={[49.872872197840614, 8.651154041290285]}
         zoom={4}
@@ -25,19 +44,31 @@ function App() {
         className="leaflet-container"
       >
         <TileLayer
-            detectRetina={true}
+          detectRetina={true}
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            //url="https://cartodb-basemaps-a.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png" // Minimalist tile
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png"
-
-
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png"
         />
-        <Marker position={[49.87384368679525, 8.631659746170046]}>
-          <Popup>
-            Studienkolleg
-          </Popup>
-        </Marker>
-        <MapClickLogger/>
+
+        {markers.map((marker, index) => (
+          <Marker key={index} position={[marker.lat, marker.lng]}>
+            <Popup>
+              Marker at [{marker.lat.toFixed(5)}, {marker.lng.toFixed(5)}]
+            </Popup>
+          </Marker>
+        ))}
+
+        {lines.map((line, index) => (
+          <Polyline
+            key={index}
+            pathOptions={{ color: "red" }}
+            positions={[
+              [line[0].lat, line[0].lng],
+              [line[1].lat, line[1].lng],
+            ]}
+          />
+        ))}
+
+        <MapClickHandler />
       </MapContainer>
     </div>
   );
